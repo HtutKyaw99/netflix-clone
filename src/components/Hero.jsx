6f03b57cfaa8cd7daad705353e9fb6d50258210b
htutkyaw <htutkyaw22dec99@gmail.com>
2024-03-23 14:41:27 +0630

    title change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,7 @@ import React, { useEffect, useState } from "react";
 
 const Hero = () => {
   const [movies, setMovies] = useState([]);
-
-  const randomMovie = () => {
-    const randomIndex = Math.floor(Math.random() * movies.length);
-    return movies[randomIndex];
-  };
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const sliceString = (str, num) => {
     if (str?.length > num) {
@@ -15,17 +11,25 @@ const Hero = () => {
     } else return str;
   };
 
-  const movie = randomMovie();
-
   useEffect(() => {
     axios
       .get(
         "https://api.themoviedb.org/3/movie/popular?api_key=1f67d42ffe4628e0ab9206c4186ae860"
       )
-      .then((res) => setMovies(res.data.results));
+      .then((res) => {
+        setMovies(res.data.results);
+      });
+
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === movies.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 7000);
+
+    return () => clearInterval(interval);
   }, []);
 
-  // console.log(movie);
+  console.log(movies[currentIndex]?.backdrop_path);
 
   return (
     <div>
@@ -34,25 +38,25 @@ const Hero = () => {
           <div className="w-full h-[550px] absolute bg-gradient-to-r from-black"></div>
           <img
             className="w-full h-full object-top object-cover"
-            src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
+            src={`https://image.tmdb.org/t/p/original${movies[currentIndex]?.backdrop_path}`}
             alt=""
           />
         </div>
         <div className="absolute text-white top-[20%] p-4">
           <h3 className="font-bold text-3xl md:text-6xl my-3">
-            {movie?.title}
+            {movies[currentIndex]?.title}
           </h3>
-          <button className="px-5 py-2 bg-gray-300 border border-gray-300 text-black hover:bg-transparent hover:text-white">
+          <button className="px-5 py-2 bg-gray-300 border border-gray-300 text-black ">
             Play
           </button>
-          <button className="ml-3 px-5 py-2 text-white border border-gray-300 hover:bg-gray-300 hover:border-0 hover:text-black ">
+          <button className="ml-3 px-5 py-2 text-white border border-gray-300 ">
             Watch Later
           </button>
           <p className="text-gray-400 mb-3 mt-6">
-            Released : {movie?.release_date}
+            Released : {movies[currentIndex]?.release_date}
           </p>
           <p className="text-gray-300 w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%]">
-            {sliceString(movie?.overview, 150)}
+            {sliceString(movies[currentIndex]?.overview, 150)}
           </p>
         </div>
       </div>
